4371aa71d1496d4d7ecc76ea0e0efe83e1c46b58
Advaith S Shetty <137682158+advaithsshetty@users.noreply.github.com>
2025-09-13 22:58:11 +0530

    Update logos and logo components
    Added Aerophilia logo SVG and component. Updated Devhost, SRC, and SSTH logo SVGs and their respective components. Modified Scroller component.

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -24,6 +24,7 @@ function ParallaxText({ children, baseVelocity = 100 }) {
   const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
 
   const directionFactor = useRef(1);
+
   useAnimationFrame((t, delta) => {
     let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
     if (velocityFactor.get() < 0) {
@@ -56,12 +57,22 @@ function ParallaxText({ children, baseVelocity = 100 }) {
 }
 
 export default function Scroller() {
+  const synergiaStartDate = new Date("2025-11-07T08:30:00");
+
+  const parallaxText = () => {
+    const currentDate = new Date();
+    if (currentDate < synergiaStartDate) return "Coming Soon";
+    else if (currentDate >= synergiaStartDate && currentDate < new Date("2025-11-09T18:00:00"))
+      return "See You In Synergia";
+    else return "See You Next Year";
+  };
+
   return (
     <div className="relative mt-40 mb-20">
       <div className="absolute left-0 top-0 h-full w-16 bg-gradient-to-r from-black to-transparent z-10" />
       <div className="absolute right-0 top-0 h-full w-16 bg-gradient-to-l from-black to-transparent z-10" />
-      <ParallaxText baseVelocity={-2}>Synergia 2024</ParallaxText>
-      <ParallaxText baseVelocity={2}>Coming Soon</ParallaxText>
+      <ParallaxText baseVelocity={-2}>Synergia 2025</ParallaxText>
+      <ParallaxText baseVelocity={2}>{parallaxText()}</ParallaxText>
     </div>
   );
 }
